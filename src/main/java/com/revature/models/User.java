39af3a5d22079ed808e107630ccf06ad95dc2bcb
nicksideal <nicolas.fajardo@revature.net>
2020-01-20 18:03:18 -0500

    creating models and trying hibernate

---
+++ src/main/java/com/revature/models/User.java
@@ -0,0 +1,142 @@
+package com.revature.models;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="USER")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="userIdGenerator")
+	@SequenceGenerator(name="userIdGenerator", sequenceName="user_id_sequence", allocationSize=1)
+	@Column(name="user_id")
+	private int id;
+	
+	@Column
+	private String firstName;
+	
+	@Column
+	private String lastName;
+	
+	@Column
+	private String username;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private int balance;
+	
+	@Column 
+	private List<pokeBall> pokeball;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int id, String firstName, String lastName, String username, String password, int balance,
+			List<pokeBall> pokeball) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.balance = balance;
+		this.pokeball = pokeball;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(balance, firstName, id, lastName, password, pokeball, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof User)) {
+			return false;
+		}
+		User other = (User) obj;
+		return balance == other.balance && Objects.equals(firstName, other.firstName) && id == other.id
+				&& Objects.equals(lastName, other.lastName) && Objects.equals(password, other.password)
+				&& Objects.equals(pokeball, other.pokeball) && Objects.equals(username, other.username);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public List<pokeBall> getPokeball() {
+		return pokeball;
+	}
+
+	public void setPokeball(List<pokeBall> pokeball) {
+		this.pokeball = pokeball;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", username=" + username
+				+ ", password=" + password + ", balance=" + balance + ", pokeball=" + pokeball + "]";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/revature/models/pokeBall.java
@@ -0,0 +1,12 @@
+package com.revature.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="PokeBall")
+public class pokeBall {
+	
+	
+
+}
