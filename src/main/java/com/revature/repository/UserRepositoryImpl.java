1078033f7ac7fc6a13530b23346c41390f7fa0a2
Monochromespecs <josmecian@gmial.com>
2020-01-24 00:32:05 -0500

    resolved logging error

--- src/main/java/com/revature/repository/UserRepositoryImpl.java
+++ src/main/java/com/revature/repository/UserRepositoryImpl.java
@@ -1,5 +1,10 @@
 package com.revature.repository;
 
+
+
+
+
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -14,17 +19,15 @@ public class UserRepositoryImpl implements UserRepository{
 	@Autowired
 	UserRepository userRepository;
 	
-	private static Logger logger = Logger.getLogger(UserRepositoryImpl.class);
+	private static final Logger LOGGER = LogManager.getLogger(UserRepositoryImpl.class);
 	
 	@Override
 	public void transaction(int amount) {
 
-		
 	}
 
 	@Override
 	public int sale(int value) {
-
 		return 0;
 	}
 
